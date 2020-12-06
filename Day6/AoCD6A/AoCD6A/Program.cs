d8546f0066e16247d4c55a7365a0d1881a4ce5f7
Mogmiester <mattalves@gmail.com>
2020-12-06 10:02:08 +0000

    Please don't look at part B

---
+++ Day6/AoCD6A/AoCD6A/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace AoCD6A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputFileLocation = @"C:\Users\matta\Documents\Advent of Code\Inputs\";
+            string InputFileName = "Day6A.txt";
+            string fileAsString = File.ReadAllText(Path.Combine(InputFileLocation, InputFileName)).Replace("\r", "");
+
+            string[] initialSplit = new string[1];
+            initialSplit[0] = "\n\n";
+            string[] customsFormsAllGroups = fileAsString.Split(initialSplit, StringSplitOptions.RemoveEmptyEntries);
+
+            HashSet<HashSet<char>> GroupAnswers = new HashSet<HashSet<char>>();
+
+            int runningTotalOfAnswers = 0;
+
+            foreach (string customFormsAnswers in customsFormsAllGroups)
+            {
+                HashSet<char> uniqueAnswers = new HashSet<char>(customFormsAnswers.Replace("\n", "").ToCharArray());
+
+                GroupAnswers.Add(uniqueAnswers);
+                runningTotalOfAnswers += uniqueAnswers.Count;
+            }
+
+            Console.WriteLine(runningTotalOfAnswers.ToString());
+
+        }
+    }
+}
---
+++ Day6/AoCD6B/AoCD6B/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace AoCD6B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputFileLocation = @"C:\Users\matta\Documents\Advent of Code\Inputs\";
+            string InputFileName = "Day6A.txt";
+            string fileAsString = File.ReadAllText(Path.Combine(InputFileLocation, InputFileName)).Replace("\r", "");
+            const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string[] initialSplit = new string[1];
+            initialSplit[0] = "\n\n";
+            string[] customsFormsAllGroups = fileAsString.Split(initialSplit, StringSplitOptions.RemoveEmptyEntries);
+
+            int runningTotalOfAnswers = 0;
+
+            foreach (string customFormsAnswers in customsFormsAllGroups)
+            {
+                HashSet<Char> GroupAnswers = new HashSet<char>(alphabet.ToCharArray());
+                string[] groupAnswers = customFormsAnswers.Split('\n');
+
+                foreach (string personYesAnswers in groupAnswers)
+                {
+                    char[] alphabetCharArray = alphabet.ToCharArray();
+                    HashSet<char> personNoAnswers = new HashSet<char>(alphabetCharArray);
+                    HashSet<char> personYesAnswersHashSet = new HashSet<char>(personYesAnswers.ToCharArray());
+                    personNoAnswers.ExceptWith(personYesAnswersHashSet);
+                    GroupAnswers.ExceptWith(personNoAnswers);
+                }
+
+                runningTotalOfAnswers += GroupAnswers.Count;
+            }
+
+            Console.WriteLine(runningTotalOfAnswers.ToString());
+
+        }
+    }
+}
