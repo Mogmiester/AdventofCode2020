def858f5161500499431f3c648a2879e41b307b2
Mogmiester <mattalves@gmail.com>
2020-12-15 00:33:31 +0000

    commit message

---
+++ Day14/AoCD14A/AoCD14A/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+
+namespace AoCD14A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputFileLocation = @"C:\Users\matta\Documents\Advent of Code\Inputs\";
+            string InputFileName = "Day14A.txt";
+            string fileAsString = File.ReadAllText(Path.Combine(InputFileLocation, InputFileName)).Replace("\r", "");
+            string[] initialSplit = { "\n" };
+            string[] splitFile = fileAsString.Split(initialSplit, StringSplitOptions.RemoveEmptyEntries);
+            int[] maskValue = new int[36];
+            bool[] maskSet = new bool[36];
+            long[] memoryValues = new long[65536];
+
+            for (int i = 0; i < splitFile.Length; i++)
+            {
+                if (splitFile[i][1] == 'a') //mask
+                {
+                    string maskString = splitFile[i].Split(" = ")[1];
+                    for (int j = 0; j < maskValue.Length; j++)
+                    {
+                        if (maskString[j] == 'X')
+                        {
+                            maskValue[j] = 0;
+                            maskSet[j] = false;
+                        }
+                        else if (maskString[j] == '0')
+                        {
+                            maskValue[j] = 0;
+                            maskSet[j] = true;
+                        }
+                        else if (maskString[j] == '1')
+                        {
+                            maskValue[j] = 1;
+                            maskSet[j] = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Panic: Unknown mask digit on line" + (i + 1).ToString());
+                        }
+                    }
+                }
+                else if (splitFile[i][1] == 'e') // memory assignment
+                {
+                    string[] split = { "] = ", "[", };
+                    string[] instructionString = splitFile[i].Split(split, StringSplitOptions.RemoveEmptyEntries);
+                    Int32.TryParse(instructionString[2], out int intialAssignmentValue);
+                    string initialBinaryString = Convert.ToString(intialAssignmentValue, 2);
+                    string leftPadding = "";
+                    for (int j = 0; j < maskValue.Length - initialBinaryString.Length; j++)
+                    {
+                        leftPadding += "0";
+                    }
+                    initialBinaryString = leftPadding + initialBinaryString;
+                    string binaryString = "";
+                    for (int j = 0; j < maskValue.Length; j++)
+                    {
+                        if (maskSet[j])
+                        {
+                            binaryString += maskValue[j];
+                        }
+                        else
+                        {
+                            binaryString += initialBinaryString[j];
+                        }
+                    }
+                    long assignmentValue = Convert.ToInt64(binaryString, 2);
+                    Int64.TryParse(instructionString[1], out long memoryLocation);
+                    memoryValues[memoryLocation] = assignmentValue;
+                }
+                else
+                {
+                    Console.WriteLine("Panic: Unknown instruction on line" + (i + 1).ToString());
+                }
+            }
+            long runningTotal = 0;
+            for (int i = 0; i < memoryValues.Length; i++)
+            {
+                runningTotal += memoryValues[i];
+            }
+            Console.WriteLine(runningTotal.ToString());
+        }
+    }
+}
---
+++ Day14/AoCD14B/AoCD14B/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoCD14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputFileLocation = @"C:\Users\matta\Documents\Advent of Code\Inputs\";
+            string InputFileName = "Day14A.txt";
+            string fileAsString = File.ReadAllText(Path.Combine(InputFileLocation, InputFileName)).Replace("\r", "");
+            string[] initialSplit = { "\n" };
+            string[] splitFile = fileAsString.Split(initialSplit, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, bool[]> maskDictionary = new Dictionary<string, bool[]>();
+            Dictionary<string, long> memoryValues = new Dictionary<string, long>();
+
+            for (int i = 0; i < splitFile.Length; i++)
+            {
+                if (splitFile[i][1] == 'a') //mask
+                {
+                    maskDictionary.Clear();
+                    maskDictionary.Add("", new bool[36]);
+                    string maskString = splitFile[i].Split(" = ")[1];
+                    for (int j = 0; j < maskString.Length; j++)
+                    {
+                        if (maskString[j] == 'X') //floating, need to double up existing strings
+                        {
+                            Dictionary<string, bool[]> newMaskDictionary = new Dictionary<string, bool[]>();
+                            foreach (KeyValuePair<string, bool[]> mask in maskDictionary)
+                            {
+                                bool[] newOverrideFlag0 = mask.Value;
+                                newOverrideFlag0[j] = true;
+                                bool[] newOverrideFlag1 = mask.Value;
+                                newOverrideFlag1[j] = true;
+                                newMaskDictionary.Add(mask.Key + "0", newOverrideFlag0);
+                                newMaskDictionary.Add(mask.Key + "1", newOverrideFlag1);
+                            }
+                            maskDictionary.Clear();
+                            maskDictionary = newMaskDictionary;
+                        }
+                        else if (maskString[j] == '1' || maskString[j] == '0')
+                        {
+                            Dictionary<string, bool[]> newMaskDictionary = new Dictionary<string, bool[]>();
+                            foreach (KeyValuePair<string, bool[]> mask in maskDictionary)
+                            {
+                                newMaskDictionary.Add(mask.Key + maskString[j], mask.Value);
+                            }
+                            maskDictionary.Clear();
+                            maskDictionary = newMaskDictionary;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Panic: Unknown mask digit on line" + (i + 1).ToString());
+                        }
+                    }
+                }
+                else if (splitFile[i][1] == 'e') // memory assignment
+                {
+                    string[] split = { "] = ", "[", };
+                    string[] instructionString = splitFile[i].Split(split, StringSplitOptions.RemoveEmptyEntries);
+                    Int64.TryParse(instructionString[2], out long assignmentValue);
+                    Int64.TryParse(instructionString[1], out long initialMemoryLocation);
+                    string initialBinaryString = Convert.ToString(initialMemoryLocation, 2);
+                    string leftPadding = "";
+                    for (int j = 0; j < maskDictionary.First().Key.Length - initialBinaryString.Length; j++)
+                    {
+                        leftPadding += "0";
+                    }
+                    initialBinaryString = leftPadding + initialBinaryString;
+                    foreach (KeyValuePair<string, bool[]> mask in maskDictionary)
+                    {
+                        string memoryLocation = "";
+                        for (int j = 0; j < mask.Key.Length; j++)
+                        {
+                            if (mask.Value[j])
+                            {
+                                memoryLocation += mask.Key[j];
+                            }
+                            else if (mask.Key[j] == '1' || initialBinaryString[j] == '1')
+                            {
+                                memoryLocation += '1';
+                            }
+                            else
+                            {
+                                memoryLocation += '0';
+                            }
+                        }
+                        memoryValues[memoryLocation] = assignmentValue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Panic: Unknown instruction on line" + (i + 1).ToString());
+                }
+            }
+            long runningTotal = 0;
+            foreach (KeyValuePair<string, long> keyValuePair in memoryValues)
+            {
+                runningTotal += keyValuePair.Value;
+            }
+            Console.WriteLine(runningTotal.ToString());
+        }
+    }
+}
